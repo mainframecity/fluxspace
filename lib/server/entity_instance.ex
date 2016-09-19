1cb05511ff1954bb0869587f1febd2c0d7cd6ab6
Andrew Vy <andrew@andrewvy.com>
2016-09-18 19:44:45 -0700

    :package: Move serialization into entity_instance.ex

--- lib/server/entity_instance.ex
+++ lib/server/entity_instance.ex
@@ -1,6 +1,18 @@
 defmodule Fluxspace.EntityInstance do
+  alias Fluxspace.EntityInstance
+  alias Fluxspace.Entity
+
   defstruct [
     uuid: "",
     attributes: %{}
   ]
+
+  def from_entity(entity_pid) do
+    entity = Entity.get_state(entity_pid)
+
+    %EntityInstance{
+      uuid: entity.state.uuid,
+      attributes: entity.state.attributes
+    } |> Poison.encode!
+  end
 end
--- lib/server/file.ex
+++ lib/server/file.ex
@@ -3,9 +3,6 @@ defmodule Fluxspace.File do
   Methods for writing to .fluxdata files.
   """
 
-  alias Fluxspace.Entity
-  alias Fluxspace.EntityInstance
-
   @file_extension ".flux"
   @data_directory "data/"
 
@@ -14,17 +11,5 @@ defmodule Fluxspace.File do
   """
   def start do
     File.mkdir_p(@data_directory)
-  end
-
-  @doc """
-  Serializes an entity.
-  """
-  def serialize(entity_pid) do
-    entity = Entity.get_state(entity_pid)
-
-    %EntityInstance{
-      uuid: entity.state.uuid,
-      attributes: entity.state.attributes
-    } |> Poison.encode!
   end
 end
