4cc22a2f004d183e7d9a1a04fa540773c12ed325
Andrew Vy <andrew@andrewvy.com>
2016-09-23 16:51:55 -0700

    :construction: Basic FOV without recursion shadowcasting

--- lib/server/fov.ex
+++ lib/server/fov.ex
@@ -9,38 +9,31 @@ defmodule Fluxspace.FOV do
   # Transformations for different FOV quadrants
 
   @directions [
-    :e_ne,
-    :ne_n,
     :n_nw,
     :nw_w,
     :w_sw,
     :sw_s,
     :s_se,
-    :se_e
+    :se_e,
+    :e_ne,
+    :ne_n
   ]
 
-  @nw_n { 1, 0, 0, -1 }
-  @w_nw { 0, 1, -1, 0 }
-
-  @e_ne { 1,  0,  0,  1}
-  @ne_n { 0,  1,  1,  0}
-  @n_nw { 0, -1,  1,  0}
-  @nw_w {-1,  0,  0,  1}
-  @w_sw {-1,  0,  0, -1}
-  @sw_s { 0, -1, -1,  0}
-  @s_se { 0,  1, -1,  0}
-  @se_e { 1,  0,  0, -1}
-
-  def denormalize_coordinates(:e_ne, coords) do
-    denormalize_coordinates(@e_ne, coords)
-  end
-
-  def denormalize_coordinates(:ne_n, coords) do
-    denormalize_coordinates(@ne_n, coords)
-  end
+  @n_nw {1, 0, 0, -1}
+  @nw_w {0, -1, 1, 0}
+  @w_sw {0, -1, -1, 0}
+  @sw_s {1, 0, 0, 1}
+  @s_se {-1, 0, 0, 1}
+  @se_e {0, 1, -1, 0}
+  @e_ne {0, 1, 1, 0}
+  @ne_n {-1, 0, 0, -1}
 
   def denormalize_coordinates(:n_nw, coords) do
     denormalize_coordinates(@n_nw, coords)
+  end
+
+  def denormalize_coordinates(:nw_w, coords) do
+    denormalize_coordinates(@nw_w, coords)
   end
 
   def denormalize_coordinates(:w_sw, coords) do
@@ -57,6 +50,14 @@ defmodule Fluxspace.FOV do
 
   def denormalize_coordinates(:se_e, coords) do
     denormalize_coordinates(@se_e, coords)
+  end
+
+  def denormalize_coordinates(:e_ne, coords) do
+    denormalize_coordinates(@e_ne, coords)
+  end
+
+  def denormalize_coordinates(:ne_n, coords) do
+    denormalize_coordinates(@ne_n, coords)
   end
 
   @doc """
@@ -89,17 +90,66 @@ defmodule Fluxspace.FOV do
     height = get_height(map)
     width = get_width(map)
 
+    light_map =
+      Range.new(0, height)
+      |> Enum.reduce([], fn(_, acc) ->
+        [Range.new(0, width)
+        |> Enum.reduce([], fn(_, acc) ->
+          [0 | acc]
+        end) | acc]
+      end)
+
     @directions
-    |> Enum.map(fn(direction) ->
-      cast_light(map, entity_position, radius)
+    |> Enum.reduce(light_map, fn(direction, acc) ->
+      merge_matrice(
+        acc,
+        scan(direction, 1, map, light_map, -1, 1, entity_position, {height, width}, radius)
+      )
     end)
-    |> overlay_light_maps()
   end
 
-  def cast_light(map, entity_position, view_radius) do
+  def merge_matrice(a1, a2) do
+    a1
+    |> Enum.with_index
+    |> Enum.reduce([], fn({a1_row, a1_row_idx}, acc) ->
+      a2_row = Enum.at(a2, a1_row_idx)
+
+      new_row =
+        a1_row
+        |> Enum.with_index
+        |> Enum.map(fn({a1_cell, col_idx}) ->
+          case a1_cell do
+            0 -> Enum.at(a2_row, col_idx)
+            1 -> 1
+          end
+        end)
+
+      acc ++ [new_row]
+    end)
   end
 
-  def overlay_light_maps(light_maps) do
-    light_maps
+  def scan(direction, current_row, map, light_map, start_slope, end_slope, {origin_x, origin_y}, {map_height, map_width}, radius) do
+    new_light_map =
+      Range.new(0, -current_row)
+      |> Enum.reduce(light_map, fn(current_column, acc) ->
+        {grid_x, grid_y} = denormalize_coordinates(direction, {current_column, current_row, origin_x, origin_y})
+
+        if (grid_x < 0) or (grid_y < 0) or (grid_x > map_width - 1) or (grid_y > map_height - 1) do
+          acc
+        else
+          tile_id =
+            map
+            |> Enum.at(grid_y)
+            |> Enum.at(grid_x)
+
+          List.update_at(acc, grid_y, &(List.update_at(&1, grid_x, fn(_) -> 1 end)))
+        end
+      end)
+
+    if current_row < radius do
+      scan(direction, current_row + 1, map, new_light_map, start_slope, end_slope, {origin_x, origin_y}, {map_height, map_width}, radius)
+    else
+      new_light_map
+    end
   end
 end
--- transform.exs
+++ transform.exs
@@ -4,26 +4,26 @@ defmodule Transform do
 
     [
       {:n_nw, {-1,  2}, {2, 1}},
-      {:nw_w, {-2,  1}, {1, 2}},
-      {:w_sw, {-2, -1}, {1, 4}},
-      {:sw_s, {-1, -2}, {2, 5}},
-      {:s_se, { 1, -2}, {4, 5}},
-      {:se_e, { 2, -1}, {5, 4}},
-      {:e_ne, { 2,  1}, {5, 2}},
-      {:ne_n, { 1,  2}, {4, 1}}
+      {:nw_w, {-1,  2}, {1, 2}},
+      {:w_sw, {-1,  2}, {1, 4}},
+      {:sw_s, {-1,  2}, {2, 5}},
+      {:s_se, {-1,  2}, {4, 5}},
+      {:se_e, {-1,  2}, {5, 4}},
+      {:e_ne, {-1,  2}, {5, 2}},
+      {:ne_n, {-1,  2}, {4, 1}}
     ]
     |> Enum.map(fn({direction, {local_x, local_y}, {global_x, global_y}}) ->
       {origin_x, origin_y} = origin
 
       [
-        { 1,  0,  0,  1},
-        { 0,  1,  1,  0},
-        { 0, -1,  1,  0},
-        {-1,  0,  0,  1},
-        {-1,  0,  0, -1},
-        { 0, -1, -1,  0},
-        { 0,  1, -1,  0},
-        { 1,  0,  0, -1}
+        {1, 0, 0, -1},
+        {-1, 0, 0, 1},
+        {-1, 0, 0, -1},
+        {0, 1, -1, 0},
+        {0, -1, -1, 0},
+        {0, 1, 1, 0},
+        {0, -1, 1, 0},
+        {1, 0, 0, 1}
       ]
       |> Enum.find(fn({xx, xy, yx, yy} = transformation) ->
          {test_x, test_y} = {
@@ -31,8 +31,13 @@ defmodule Transform do
            (origin_y + (local_x * yx) + (local_y * yy)),
          }
 
-         if (test_x == global_x and test_y == global_y) do
-           IO.inspect({direction, transformation})
+         if test_x == global_x and test_y == global_y do
+           IO.inspect({
+             direction,
+             transformation,
+             test_x,
+             test_y
+           })
          end
       end)
     end)
