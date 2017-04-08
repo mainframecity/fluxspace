af6e646ff3f979544bf64e77f32f56e107c3f75a
Andrew Vy <andrew@andrewvy.com>
2017-04-07 19:37:05 -0700

    :books: Add logging

--- lib/lib/entrypoints/websocket/server.ex
+++ lib/lib/entrypoints/websocket/server.ex
@@ -1,8 +1,12 @@
 defmodule Fluxspace.Entrypoints.Websocket.Server do
+  @port 4050
+
   def start_link(_state, _args) do
     dispatch_config = build_dispatch_config()
 
-    :cowboy.start_http(:http, 100, [{:port, 4050}], [{:env, [{:dispatch, dispatch_config}]}])
+    IO.puts("Started Websocket server on #{@port}")
+
+    :cowboy.start_http(:http, 100, [{:port, @port}], [{:env, [{:dispatch, dispatch_config}]}])
   end
 
   def build_dispatch_config() do
